ca507038e9453324a740a29d11f561bbad96025e
brenttsai1148 <brenttsai1148@gmail.com>
2026-06-18 11:18:16 +0800

    test 南投遊戲

---
+++ sample/Nantou2025_1120/src/settings.9a063.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/Scene/Game.fire",orientation:"",jsList:[],bundleVers:{internal:"d0832",resources:"fdbc0",main:"2e8c8"}};
